afafb717446ba85c3c54b8a832977332aa365560
administrator <bazu_21@yahoo.com>
2019-09-02 21:05:40 +0300

    add fields,some basic implementation and lombok annotation

--- src/main/java/com/sda/vendingmachine/bank/Bank.java
+++ src/main/java/com/sda/vendingmachine/bank/Bank.java
@@ -1,4 +1,16 @@
 package com.sda.vendingmachine.bank;
 
+import java.util.Queue;
+
 public class Bank {
+
+    //STACK - LIFO - LAST IN FIRST OUT
+    //QUEUE - FIFO - FIRST IN FIRST OUT
+
+    private Queue<Coin> pennyStack;
+    private Queue<Coin> nickelStack;
+    private Queue<Coin> dimeStack;
+    private Queue<Coin> halfStack;
+    private Queue<Coin> quarterStack;
+    private Queue<Coin> oneDollarStack;
 }
